d29b2ce270dd4673213363ccf50f54f6bb87a17b
Riley Testut <riley@rileytestut.com>
2014-12-26 02:37:22 -0600

    Added toolbar with buttons to go forward, go back, refresh, stop loading, and share link

--- RSTWebViewController/RSTWebViewController/RSTWebViewController.swift
+++ RSTWebViewController/RSTWebViewController/RSTWebViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 import WebKit
 
+public extension RSTWebViewController {
+    
+    //MARK: Update UI
+    
+    func updateToolbarItems()
+    {
+        if self.webView.loading
+        {
+            self.refreshButton = self.stopLoadingButton
+        }
+        else
+        {
+            self.refreshButton = self.reloadButton
+        }
+        
+        self.backButton.enabled = self.webView.canGoBack
+        self.forwardButton.enabled = self.webView.canGoForward
+        
+        self.toolbarItems = [self.backButton, self.flexibleSpaceItem, self.forwardButton, self.flexibleSpaceItem, self.refreshButton, self.flexibleSpaceItem, self.shareButton]
+    }
+    
+}
+
 public class RSTWebViewController: UIViewController {
     
     //MARK: Public Properties
     
     // WKWebView used to display webpages
-    public private(set) var webView: WKWebView!
+    public private(set) var webView: WKWebView
+    
+    public private(set) var refreshButton: UIBarButtonItem
+    public let backButton: UIBarButtonItem = UIBarButtonItem(image: nil, style: .Plain, target: nil, action: "goBack:")
+    public let forwardButton: UIBarButtonItem = UIBarButtonItem(image: nil, style: .Plain, target: nil, action: "goForward:")
+    public let shareButton: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Action, target: nil, action: "shareLink:")
+    public let flexibleSpaceItem: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: .FlexibleSpace, target: nil, action: nil)
     
     // Set to true when presenting modally to show a Done button that'll dismiss itself. Must be set before presentation.
     public var showsDoneButton: Bool = false
@@ -23,7 +52,11 @@ public class RSTWebViewController: UIViewController {
     //MARK: Private Properties
     
     private let initialReqest: NSURLRequest?
-    private let progressView: UIProgressView
+    private let progressView = UIProgressView()
+    private var ignoreUpdateProgress: Bool = false
+    
+    private let reloadButton: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Refresh, target: nil, action: "refresh:")
+    private let stopLoadingButton: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Stop, target: nil, action: "refresh:")
     
     
     //MARK: Initializers
@@ -32,7 +65,10 @@ public class RSTWebViewController: UIViewController {
     {
         self.initialReqest = request
         
-        self.progressView = UIProgressView()
+        let configuration = WKWebViewConfiguration()
+        self.webView = WKWebView(frame: CGRectZero, configuration: configuration)
+        
+        self.refreshButton = self.reloadButton
         
         super.init(nibName: nil, bundle: nil)
         
@@ -65,7 +101,10 @@ public class RSTWebViewController: UIViewController {
     
     public required init(coder: NSCoder)
     {
-        self.progressView = UIProgressView()
+        let configuration = WKWebViewConfiguration()
+        self.webView = WKWebView(frame: CGRectZero, configuration: configuration)
+        
+        self.refreshButton = self.reloadButton
         
         super.init(coder: coder)
         
@@ -79,6 +118,16 @@ public class RSTWebViewController: UIViewController {
         self.progressView.progress = 0.5
         self.progressView.alpha = 0.0
         self.progressView.hidden = true
+        
+        self.backButton.target = self
+        self.forwardButton.target = self
+        self.reloadButton.target = self
+        self.stopLoadingButton.target = self
+        self.shareButton.target = self
+        
+        let bundle = NSBundle(forClass: RSTWebViewController.self)
+        self.backButton.image = UIImage(named: "back_button", inBundle: bundle, compatibleWithTraitCollection: nil)
+        self.forwardButton.image = UIImage(named: "forward_button", inBundle: bundle, compatibleWithTraitCollection: nil)
     }
     
     deinit
@@ -91,9 +140,6 @@ public class RSTWebViewController: UIViewController {
     
     public override func loadView()
     {
-        let configuration = WKWebViewConfiguration()
-        self.webView = WKWebView(frame: CGRectZero, configuration: configuration)
-        
         self.startKeyValueObserving()
         
         if let request = self.initialReqest
@@ -113,6 +159,8 @@ public class RSTWebViewController: UIViewController {
             let doneButton = UIBarButtonItem(barButtonSystemItem: .Done, target: self, action: "dismissWebViewController:")
             self.navigationItem.setRightBarButtonItem(doneButton, animated: false)
         }
+        
+        self.updateToolbarItems()
     }
     
     public override func viewWillAppear(animated: Bool)
@@ -133,11 +181,28 @@ public class RSTWebViewController: UIViewController {
                     }
             }
         }
+        
+        self.navigationController?.setToolbarHidden(false, animated: false)
     }
     
     public override func viewWillDisappear(animated: Bool)
     {
         super.viewDidDisappear(animated)
+        
+        var shouldHideToolbarItems = true
+        
+        if let toolbarItems = self.navigationController?.topViewController.toolbarItems
+        {
+            if toolbarItems.count > 0
+            {
+                shouldHideToolbarItems = false
+            }
+        }
+        
+        if shouldHideToolbarItems
+        {
+            self.navigationController?.setToolbarHidden(true, animated: false)
+        }
         
         self.transitionCoordinator()?.animateAlongsideTransition( { (context) in
             
@@ -165,14 +230,6 @@ public class RSTWebViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
-    //MARK: Dismissal
-    
-    // Cannot be private or else it will crash upon being called ಠ_ಠ
-    internal func dismissWebViewController(sender: UIBarButtonItem)
-    {
-        self.parentViewController?.dismissViewControllerAnimated(true, completion: nil)
-    }
-    
     //MARK: KVO
     
     public override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<()>)
@@ -192,6 +249,9 @@ public class RSTWebViewController: UIViewController {
             case "loading":
                 self.updateLoadingStatus(status: webView.loading)
                 
+            case "canGoBack", "canGoForward":
+                self.updateToolbarItems()
+                
             default:
                 println("Unknown KVO keypath")
             }
@@ -204,6 +264,80 @@ public class RSTWebViewController: UIViewController {
 
 }
 
+// Cannot be private or else they will crash upon being called ಠ_ಠ
+internal extension RSTWebViewController {
+    
+    //MARK: Dismissal
+    
+    func dismissWebViewController(sender: UIBarButtonItem)
+    {
+        self.parentViewController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    //MARK: Toolbar Items
+    
+    func goBack(button: UIBarButtonItem)
+    {
+        self.webView.goBack()
+    }
+    
+    func goForward(button: UIBarButtonItem)
+    {
+        self.webView.goForward()
+    }
+    
+    func refresh(button: UIBarButtonItem)
+    {
+        if self.webView.loading
+        {
+            self.ignoreUpdateProgress = true
+            self.webView.stopLoading()
+        }
+        else
+        {
+            if self.webView.URL == nil && self.webView.backForwardList.backList.count == 0 && self.initialReqest != nil
+            {
+                self.webView.loadRequest(self.initialReqest!)
+            }
+            else
+            {
+                self.webView.reload()
+            }
+        }
+    }
+    
+    func shareLink(button: UIBarButtonItem)
+    {
+        let reloadButtonTintColor = self.reloadButton.tintColor
+        let stopLoadingButtonTintColor = self.stopLoadingButton.tintColor
+        
+        let activityViewController = UIActivityViewController(activityItems: [self.webView.URL ?? ""], applicationActivities: nil)
+        activityViewController.completionWithItemsHandler = { activityType, success, items, error in
+            
+            // Because tint colors aren't properly updated when views aren't in a view hierarchy, we manually fix any erroneous tint colors
+            self.progressView.tintColorDidChange()
+            
+            let systemTintColor = UIColor(red: 0, green: 0.478431, blue: 1, alpha: 1)
+            
+            // If previous tint color is nil, we need to temporarily set the tint color to something else or it won't visually update the tint color
+            if reloadButtonTintColor == nil
+            {
+                self.reloadButton.tintColor = systemTintColor
+            }
+            
+            if stopLoadingButtonTintColor == nil
+            {
+                self.stopLoadingButton.tintColor = systemTintColor
+            }
+            
+            self.reloadButton.tintColor = reloadButtonTintColor
+            self.stopLoadingButton.tintColor = stopLoadingButtonTintColor
+            
+        }
+        self.presentViewController(activityViewController, animated: true, completion: nil)
+    }
+}
+
 private let RSTWebViewControllerContext = UnsafeMutablePointer<()>()
 
 private extension RSTWebViewController {
@@ -212,22 +346,20 @@ private extension RSTWebViewController {
     
     func startKeyValueObserving()
     {
-        if let webView = self.webView
-        {
-            webView.addObserver(self, forKeyPath: "title", options:nil, context: RSTWebViewControllerContext)
-            webView.addObserver(self, forKeyPath: "estimatedProgress", options: nil, context: RSTWebViewControllerContext)
-            webView.addObserver(self, forKeyPath: "loading", options: nil, context: RSTWebViewControllerContext)
-        }
+        self.webView.addObserver(self, forKeyPath: "title", options:nil, context: RSTWebViewControllerContext)
+        self.webView.addObserver(self, forKeyPath: "estimatedProgress", options: nil, context: RSTWebViewControllerContext)
+        self.webView.addObserver(self, forKeyPath: "loading", options: nil, context: RSTWebViewControllerContext)
+        self.webView.addObserver(self, forKeyPath: "canGoBack", options: nil, context: RSTWebViewControllerContext)
+        self.webView.addObserver(self, forKeyPath: "canGoForward", options: nil, context: RSTWebViewControllerContext)
     }
     
     func stopKeyValueObserving()
     {
-        if let webView = self.webView
-        {
-            webView.removeObserver(self, forKeyPath: "title", context: RSTWebViewControllerContext)
-            webView.removeObserver(self, forKeyPath: "estimatedProgress", context: RSTWebViewControllerContext)
-            webView.removeObserver(self, forKeyPath: "loading", context: RSTWebViewControllerContext)
-        }
+        self.webView.removeObserver(self, forKeyPath: "title", context: RSTWebViewControllerContext)
+        self.webView.removeObserver(self, forKeyPath: "estimatedProgress", context: RSTWebViewControllerContext)
+        self.webView.removeObserver(self, forKeyPath: "loading", context: RSTWebViewControllerContext)
+        self.webView.removeObserver(self, forKeyPath: "canGoBack", context: RSTWebViewControllerContext)
+        self.webView.removeObserver(self, forKeyPath: "canGoForward", context: RSTWebViewControllerContext)
     }
     
     //MARK: Update UI
@@ -239,6 +371,8 @@ private extension RSTWebViewController {
     
     func updateLoadingStatus(status loading: Bool)
     {
+        self.updateToolbarItems()
+        
         if loading
         {
             UIApplication.startAnimatingNetworkActivityIndicator()
@@ -256,10 +390,16 @@ private extension RSTWebViewController {
             self.showProgressBar(animated: true)
         }
         
-        // If progress is less than self.progressView.progress, another webpage began to load before the first one completed
-        // In this case, we set the progress back to 0.0, and then wait until the next updateProgress, because it results in a much better animation
-        if progress < self.progressView.progress
+        if self.ignoreUpdateProgress
         {
+            self.ignoreUpdateProgress = false
+            self.hideProgressBar(animated: true)
+        }
+        else if progress < self.progressView.progress
+        {
+            // If progress is less than self.progressView.progress, another webpage began to load before the first one completed
+            // In this case, we set the progress back to 0.0, and then wait until the next updateProgress, because it results in a much better animation
+            
             self.progressView.setProgress(0.0, animated: false)
         }
         else
